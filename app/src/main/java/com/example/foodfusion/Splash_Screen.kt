63525aa04766c954692603664a8a824de040aa64
vipingoyat <e21cseu0694@bennett.edu.in>
2024-07-11 16:56:22 +0530

    LogIn and SignIn Activity

--- app/src/main/java/com/example/foodfusion/Splash_Screen.kt
+++ app/src/main/java/com/example/foodfusion/Splash_Screen.kt
@@ -3,18 +3,17 @@ package com.example.foodfusion
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-//hi
-@Suppress("DEPRECATION")
 class Splash_Screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this,StartActivity::class.java)
             startActivity(intent)
             finish()
